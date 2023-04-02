200f526474f21f5cd3e2509cfd891c4afc50ee2e
DezSlav <huzvaviacheslav@gmail.com>
2023-04-02 18:17:51 +0300

    14

--- hw9/option1.py
+++ hw9/option1.py
@@ -1,8 +1,13 @@
-filename = "input.txt"
+def transform_lines(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
 
-with open(filename, "r") as f:
-    lines = f.readlines()
+    transformed_lines = [line.strip("\n").split("a", 1)[-1].capitalize() if "a" in line else "" for line in lines]
 
-transformed_lines = [line.strip("\n").split("a", 1)[-1].capitalize() if "a" in line else "" for line in lines]
+    return transformed_lines
 
-print(transformed_lines)
+
+if __name__ == "__main__":
+    filename = "input.txt"
+    transformed_lines = transform_lines(filename)
+    print(transformed_lines)
