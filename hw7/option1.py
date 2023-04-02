9bf75067a1e481b9f6a9ef37910b502a9bbcab7e
DezSlav <huzvaviacheslav@gmail.com>
2023-04-02 18:29:44 +0300

    14

--- hw7/option1.py
+++ hw7/option1.py
@@ -1,42 +1,60 @@
-if __name__ == "__main__":
-    notes = []
-
-while True:
-    # Виводимо повідомлення користувачу та чекаємо введення команди
-    command = input("Enter command (add/earliest/latest/longest/shortest/exit): ")
-
-    if command == "add":
-        # Запитуємо користувача про текст нотатки
-        note = input("Enter note: ")
-        # Перевіряємо, що текст нотатки не містить цифри
-        if not note.isdigit():
-            # Додаємо нотатку до списку нотаток
-            notes.append(note)
-        else:
-            # Виводимо повідомлення про помилку, якщо текст нотатки містить цифри
-            print("Invalid note.")
-    elif command == "earliest":
-        # Виводимо нотатки у хронологічному порядку - від найранішої до найпізнішої
-        for note in notes:
-            print(note)
-    elif command == "latest":
-        # Виводимо нотатки у хронологічному порядку - від найпізнішої до найранішої
-        for note in reversed(notes):
-            print(note)
-    elif command == "longest":
-        # Виводимо нотатки у порядку їх довжини - від найдовшої до найкоротшої
-        sorted_notes = sorted(notes, key=len, reverse=True)
-        for note in sorted_notes:
-            print(note)
-    elif command == "shortest":
-        # Виводимо нотатки у порядку їх довжини - від найкоротшоїдо найдовшої
-        sorted_notes = sorted(notes, key=len)
-        for note in sorted_notes:
-            print(note)
-    elif command == "exit":
-        # Виводимо повідомлення про вихід з програми та виходимо з циклу
-        print("Exiting program...")
-        break
+def add_note(notes):
+    """Функція додавання нової нотатки в список notes."""
+    note = input("Enter note: ")
+    if not note.isdigit():
+        notes.append(note)
     else:
-        # Виводимо повідомлення про помилку, якщо користувач ввів неправильну команду
-        print("Invalid command.")
+        print("Invalid note.")
+    return notes
+
+
+def show_earliest(notes):
+    """Функція виведення всіх нотаток в хронологічному порядку (від найранішої до найпізнішої)."""
+    for note in notes:
+        print(note)
+
+
+def show_latest(notes):
+    """Функція виведення всіх нотаток в зворотньому хронологічному порядку (від найпізнішої до найранішої)."""
+    for note in reversed(notes):
+        print(note)
+
+
+def show_longest(notes):
+    """Функція виведення всіх нотаток у порядку їх довжини (від найдовшої до найкоротшої)."""
+    sorted_notes = sorted(notes, key=len, reverse=True)
+    for note in sorted_notes:
+        print(note)
+
+
+def show_shortest(notes):
+    """Функція виведення всіх нотаток у порядку їх довжини (від найкоротшої до найдовшої)."""
+    sorted_notes = sorted(notes, key=len)
+    for note in sorted_notes:
+        print(note)
+
+
+def main():
+    """Основна функція програми."""
+    notes = []
+    while True:
+        command = input("Enter command (add/earliest/latest/longest/shortest/exit): ")
+        if command == "add":
+            notes = add_note(notes)
+        elif command == "earliest":
+            show_earliest(notes)
+        elif command == "latest":
+            show_latest(notes)
+        elif command == "longest":
+            show_longest(notes)
+        elif command == "shortest":
+            show_shortest(notes)
+        elif command == "exit":
+            print("Exiting program...")
+            break
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()
